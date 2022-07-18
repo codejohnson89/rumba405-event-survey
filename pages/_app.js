5743223ef5f7ec8f58e0d341a8a100d066e06427
micahjj1989 <micahjj1989@gmail.com>
2022-07-17 22:42:14 -0500

    Finished product

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
---
+++ pages/util/email.js
@@ -0,0 +1,7 @@
+const keys = {
+    user_ID: 'wKGy-xV57uh5FYP28',
+    token: 'oIFp6JghG6_bhibNN-Wwl',
+    templateID: 'template_epi8d1k'
+}
+
+export default keys;
